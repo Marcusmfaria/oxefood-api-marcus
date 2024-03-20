ca064172be23a2d33186d702eb57e59eaf4c6b6a
Marcusmfaria <87538735+Marcusmfaria@users.noreply.github.com>
2024-03-20 00:04:16 -0300

    Implementando o create da api

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/Entregador/EntregadorRepository.java
@@ -0,0 +1,8 @@
+package br.com.ifpe.oxefood.modelo.Entregador;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EntregadorRepository extends JpaRepository<Entregador, Long> {
+}
--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -2,18 +2,30 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-
-@Getter
-@Setter
+@SuppressWarnings("serial")
+@Data
 @EqualsAndHashCode(of = { "id" })
-public class EntidadeNegocio implements Serializable {
-    
-    private Long id;
-    private Boolean habilitado;
+@MappedSuperclass
+public abstract class EntidadeNegocio implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @JsonIgnore
+    @Column
+    private Boolean habilitado;
 
 }
